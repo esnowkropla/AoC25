545e05258e255a7fa0c577e469c50aac7dc19366
Elliot Snow-Kropla <elliot@ejsk.ca>
2025-12-25 09:12:34 -0400

    Day 3 part 2

--- elixir/lib/day3.ex
+++ elixir/lib/day3.ex
@@ -1,27 +1,42 @@
 defmodule Aoc25.Day3 do
-  def largest_pair(bank) do
+  def largest_subsequence(bank, sequence_size) do
     digits = get_digits(bank)
-    full_count = Enum.count(digits)
-    indexed_digits = Enum.zip(digits, 0..full_count)
-
-    {first_digit, idx} =
-      indexed_digits
-      |> Enum.take(full_count - 1)
-      |> Enum.max_by(fn {digit, _idx} ->
-        digit
-      end)
-
-    second_digit =
-      Enum.drop(digits, idx + 1)
-      |> Enum.max()
-
-    first_digit * 10 + second_digit
+    find_digits(digits, 1, sequence_size, [])
   end
 
-  defp get_digits(bank) do
+  def get_digits(bank) do
     bank
     |> Integer.parse()
     |> then(fn {num, _} -> num end)
     |> Integer.digits()
+  end
+
+  def combine(digits) do
+    digits
+    |> Enum.zip(0..Enum.count(digits))
+    |> Enum.map(fn {digit, power} ->
+      digit * 10 ** power
+    end)
+    |> Enum.sum()
+  end
+
+  def find_digits(digits, n, n, acc) do
+    digits
+    |> Enum.max()
+    |> then(fn last_digit -> [last_digit | acc] end)
+    |> combine()
+  end
+
+  def find_digits(digits, n, sequence_size, acc) do
+    count = Enum.count(digits)
+    indexed_digits = Enum.zip(digits, 0..count)
+
+    {next_digit, idx} =
+      indexed_digits
+      |> Enum.take(count - sequence_size + n)
+      |> Enum.max_by(fn {digit, _idx} -> digit end)
+
+    remaining_digits = Enum.drop(digits, idx + 1)
+    find_digits(remaining_digits, n + 1, sequence_size, [next_digit | acc])
   end
 end
--- elixir/test/day3_test.exs
+++ elixir/test/day3_test.exs
@@ -3,29 +3,29 @@ defmodule Aoc25.Day3Test do
 
   import Aoc25.Day3
 
-  describe "#largest_pair" do
+  describe "#largest_subsequence" do
     test "first example" do
       bank = "987654321111111"
 
-      assert largest_pair(bank) == 98
+      assert largest_subsequence(bank, 12) == 987_654_321_111
     end
 
     test "second example" do
       bank = "811111111111119"
 
-      assert largest_pair(bank) == 89
+      assert largest_subsequence(bank, 12) == 811_111_111_119
     end
 
     test "third example" do
       bank = "234234234234278"
 
-      assert largest_pair(bank) == 78
+      assert largest_subsequence(bank, 12) == 434_234_234_278
     end
 
     test "fourth example" do
       bank = "818181911112111"
 
-      assert largest_pair(bank) == 92
+      assert largest_subsequence(bank, 12) == 888_911_112_111
     end
   end
 end
